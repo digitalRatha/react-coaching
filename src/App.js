12f9c101494d3e1aeee650cfe4dbf1f702a62b59
Manoj936 <info.digitalratha@gmail.com>
2022-03-21 21:31:12 +0530

    react-router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Bundle from './components/Bundle'
 import './App.css';
 export default function App() {
+
   return (
     <>
     <Bundle/>
--- src/components/Bundle.js
+++ src/components/Bundle.js
@@ -2,21 +2,38 @@ import React, { useState } from "react";
 import Alert from "./Alert";
 import Footer from "./Footer";
 import Home from "./Home";
+import Learnmore from "./Learnmore";
 import NavBar from "./NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function Bundle() {
-const [alertDataFromHome, setAlertDataFromHome] = useState(null)
+  const [alertDataFromHome, setAlertDataFromHome] = useState(null);
 
-
-const planSelected = (data) =>{
-  setAlertDataFromHome(data)
-}
+  const planSelected = (data) => {
+    setAlertDataFromHome(data);
+  };
   return (
-    <div className="container-fluid">
-      <NavBar />
-      <Alert alertData={alertDataFromHome}/>
-      <Home freePlan ={{plan: 15 , name : 'Basic'}} pro={{plan: 25 , name : 'Pro'}} enterprise ={{plan: 30 , name : 'Enterprise'}} selectPlan = {planSelected} />
-      <Footer />
-    </div>
+    <Router>
+      <div className="container-fluid">
+        <NavBar />
+        <Alert alertData={alertDataFromHome} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <Home
+                freePlan={{ plan: 15, name: "Basic" }}
+                pro={{ plan: 25, name: "Pro" }}
+                enterprise={{ plan: 30, name: "Enterprise" }}
+                selectPlan={planSelected}
+              />
+            }
+          ></Route>
+          <Route exact path="/learnmore" element={<Learnmore />}></Route>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
